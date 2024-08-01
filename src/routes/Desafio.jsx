2f6b1397d3e2272c441989d1e9d1dd4440779aa3
lobmar146 <lobmar146@gmail.com>
2024-08-01 11:51:01 -0300

    [Feat] Refactor Desafios + Props

--- src/routes/Desafio.jsx
+++ src/routes/Desafio.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import BlocklyComponent from '../components/BocklyComponent'
 import '../App.css'
+import { Link } from 'react-router-dom'
 
 const App = ({ titulo, consigna, toolBox }) => {
   return (
     <div className='App'>
+      <Link to='/'>Volver a los niveles</Link>
       <h1>{titulo}</h1>
-      <p>{consigna}</p>
+      <p className='consigna'>{consigna}</p>
       <BlocklyComponent toolBoxDesafio={toolBox} />
     </div>
   )
