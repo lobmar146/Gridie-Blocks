86c62635ec40c8be67678262848d3621572702ca
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2024-10-23 09:14:16 -0300

    chore(ui): add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,53 @@ import React from 'react'
 import BlocklyComponent from './components/BocklyComponent'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from '@/components/theme-provider'
 import Desafio from './routes/Desafio'
 import Home from './routes/Home'
 
 const App = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/Ejercicio1"
-          element={
-            <Desafio
-              titulo="Ejercicio 1: Encender un Led"
-              consigna={
-                'Como primer paso deberemos encender un led. Ayudas a la placa Arduino a encender el led?'
-              }
-              toolBox="1"
-            />
-          }
-        />
-        <Route
-          path="/Problema1"
-          element={
-            <Desafio
-              titulo="Ejercicio1: Parpadear"
-              consigna={
-                'Ahora queremos que parpadee. Puedes ayudar a la placa Arduino a hacerlo?'
-              }
-              toolBox="2"
-            />
-          }
-        />
-        <Route
-          path="/Ejercicio2"
-          element={
-            <Desafio
-              titulo="Ejercicio 2: Encender el Semaforo"
-              consigna={'¿Cómo hacemos para encender A-B-C en secuencia?'}
-              toolBox="3"
-            />
-          }
-        />
-      </Routes>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/Ejercicio1"
+            element={
+              <Desafio
+                titulo="Ejercicio 1: Encender un Led"
+                consigna={
+                  'Como primer paso deberemos encender un led. Ayudas a la placa Arduino a encender el led?'
+                }
+                toolBox="1"
+              />
+            }
+          />
+          <Route
+            path="/Problema1"
+            element={
+              <Desafio
+                titulo="Ejercicio1: Parpadear"
+                consigna={
+                  'Ahora queremos que parpadee. Puedes ayudar a la placa Arduino a hacerlo?'
+                }
+                toolBox="2"
+              />
+            }
+          />
+          <Route
+            path="/Ejercicio2"
+            element={
+              <Desafio
+                titulo="Ejercicio 2: Encender el Semaforo"
+                consigna={'¿Cómo hacemos para encender A-B-C en secuencia?'}
+                toolBox="3"
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </ThemeProvider>
   )
 }
 
